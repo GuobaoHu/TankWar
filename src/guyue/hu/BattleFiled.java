01edd40fd2f751c9ce96f6093483533d48a167d5
guyuehu <398456458@qq.com>
2017-11-29 21:29:19 +0800

    TankWar 0.1 初始化项目及原始界面

---
+++ src/guyue/hu/BattleFiled.java
@@ -0,0 +1,20 @@
+package guyue.hu;
+
+import java.awt.Frame;
+
+/**
+ * @author guyue
+ * @date 2017年11月29日 下午9:21:28
+ * @class describ:
+ */
+public class BattleFiled extends Frame {
+
+	public static void main(String[] args) {
+		new BattleFiled().launch();
+	}
+	
+	public void launch() {
+		this.setBounds(200, 100, 800, 600);
+		this.setVisible(true);
+	}
+}
